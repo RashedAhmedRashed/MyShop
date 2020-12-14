1e23c21e598ad042e71bbba1bd55b9418b83af86
Rashid Ahmed Rashid <rashed.rar61@gmail.com>
2020-12-14 19:23:05 +0300

    Commiting Models And Storage

---
+++ MyShop/MyShop.Core/Models/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Products
+    {
+        public string Id { get; set; }
+
+        [StringLength(20)]
+        [Display(Description ="Product Name")]
+        public string  Name { get; set; }
+
+        [Range(0,1000)]
+        public int Price { get; set; }
+        public string Category { get; set; }
+        public string  Description { get; set; }
+
+        public Products()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
