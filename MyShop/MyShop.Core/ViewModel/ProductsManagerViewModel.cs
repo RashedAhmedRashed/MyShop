1ad06c8864d20dd628ef3b67335a8209ac7f0d75
Rashid Ahmed Rashid <rashed.rar61@gmail.com>
2020-12-21 21:08:20 +0300

    Product Grouping2

---
+++ MyShop/MyShop.Core/ViewModel/ProductsManagerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModel
+{
+    public class ProductsManagerViewModel
+    {
+        public Products  product { get; set; }
+        public IEnumerable<ProductsCategories> productsCategories { get; set; }
+    }
+}
